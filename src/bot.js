d6a68f88ca30087f13805ece6ee6add495af96b2
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-08-15 08:09:02 -0400

    version saludo español o ingle y tomando en cuenta el tiempo

--- src/bot.js
+++ src/bot.js
@@ -4,33 +4,48 @@ document.addEventListener("DOMContentLoaded", function() {
     var nombre = document.getElementById("nombre").value;
     var genero = document.getElementById("genero").value;
     var edad = parseInt(document.getElementById("edad").value);
-    var idioma = document.getElementById("idioma").value;
+    var idioma =  document.getElementById("idioma").value
     var saludoElement = document.getElementById("saludo");
 
-    var saludo = "";
+    var saludo = "¡";
 
-    if (idioma === "espanol") {
-      if (edad > 30) {
-        if (genero === "masculino") {
-          saludo = "¡Hola, Señor " + nombre + "!";
-        } else if (genero === "femenino") {
-          saludo = "¡Hola, Señora " + nombre + "!";
-        }
-      } else {
-        saludo = "¡Hola, " + nombre + "!";
-      }
-    } else if (idioma === "ingles") {
-      if (edad > 30) {
-        if (genero === "masculino") {
-          saludo = "Hello, Mr. " + nombre + "!";
-        } else if (genero === "femenino") {
-          saludo = "Hello, Mrs. " + nombre + "!";
-        }
-      } else {
-        saludo = "Hello, " + nombre + "!";
-      }
+    var horaActual = new Date().getHours();
+
+    if(idioma === "espanol"){
+    if (horaActual >= 5 && horaActual < 12) {
+      saludo += "Buenos días, ";
+    } else if (horaActual >= 12 && horaActual < 18) {
+      saludo += "Buenas tardes, ";
+    } else {
+      saludo += "Buenas noches, ";
     }
 
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludo += "Señor ";
+      } else if (genero === "femenino") {
+        saludo += "Señora ";
+      }
+    }
+  }else if(idioma === "ingles"){
+    if (horaActual >= 5 && horaActual < 12) {
+      saludo += "good morning, ";
+    } else if (horaActual >= 12 && horaActual < 18) {
+      saludo += "good afternoon, ";
+    } else {
+      saludo += "good night, ";
+    }
+
+    if (edad > 30) {
+      if (genero === "masculino") {
+        saludo += "Mr ";
+      } else if (genero === "femenino") {
+        saludo += "Ms ";
+      }
+    }
+  }
+  
+    saludo += nombre + "!";
     saludoElement.textContent = saludo;
   });
 });
