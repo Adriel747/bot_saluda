42282045c5d89fa95853202b7882ef7ce402f1e5
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-08-15 07:41:37 -0400

    añade opcion de genero y segun respuesta responde

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,16 @@
-var nombre = prompt(" ingresa tu nombre:");
-var saludoElement = document.getElementById("saludo");
-saludoElement.textContent = "¡Hola, " + nombre + "!";
+document.addEventListener("DOMContentLoaded", function() {
+  var saludarButton = document.getElementById("saludarButton");
+  saludarButton.addEventListener("click", function() {
+    var nombre = document.getElementById("nombre").value;
+    var genero = document.getElementById("genero").value;
+    var saludoElement = document.getElementById("saludo");
+    
+    if (genero === "masculino") {
+      saludoElement.textContent = "¡Hola, Señor " + nombre + "!";
+    } else if (genero === "femenino") {
+      saludoElement.textContent = "¡Hola, Señora " + nombre + "!";
+    } else {
+      saludoElement.textContent = "¡Hola genero fluido, " + nombre + "!";
+    }
+  });
+});
